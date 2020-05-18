88b6dea200dda0b086c48be155c023c695dfd478
ardesp <belloshehu1@gmail.com>
2020-05-18 07:19:11 +0100

    Category and Service models created

--- smartcity/fullstack/models.py
+++ smartcity/fullstack/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    """ Model for service categories"""
+    name = models.CharField(max_length=100)
+    document_required = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Service(models.Model):
+    """ Model for services rendered"""
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.Foreignkey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
