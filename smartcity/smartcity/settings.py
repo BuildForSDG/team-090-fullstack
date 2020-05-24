94f8db12130af0a4acd217755a5b48a2c9fc678c
ardesp <belloshehu1@gmail.com>
2020-05-24 16:55:10 +0100

    phonenumber_field added

--- smartcity/smartcity/settings.py
+++ smartcity/smartcity/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fullstack',
-    'cities_light'
+    'cities_light',
+    'phonenumber_field'
 ]
 
 MIDDLEWARE = [
