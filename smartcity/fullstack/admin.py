e73771fed3f03588dff41a984d1a9b58bffc4c28
ardesp <belloshehu1@gmail.com>
2020-05-24 16:10:11 +0100

    Service, Category, ServiceProvider and CustomerProfile models added

--- smartcity/fullstack/admin.py
+++ smartcity/fullstack/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+from cities_light.models import City, Region, Country
 
 # Register your models here.
+
+admin.site.register(Service)
+admin.site.register(Category)
+admin.site.register(ServiceProvider)
+admin.site.register(CustomerProfile)
