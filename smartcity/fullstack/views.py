8700c8ce729776f08c109f0ef2dfdd32d8a5fd9a
Nextafari <chinedue16@gmail.com>
2020-05-25 00:27:30 +0100

    Feat::snake:modified views

--- smartcity/fullstack/views.py
+++ smartcity/fullstack/views.py
@@ -3,35 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    """This is the view for the index page."""
+    """View function for the index page."""
     return render(request, '.html', {})
 
 
 def login(request):
-    """This is the view for the login page."""
+    """View function for the login page."""
     return render(request, '.html', {})
 
 
 def service_provider_profile(request):
-    """This is the view for the service provider's profile page."""
+    """View function for the service provider's profile page."""
     return render(request, '.html', {})
 
 
 def customer_profile(request):
-    """This is the view for the customer's profile page."""
+    """View function for the customer's profile page."""
     return render(request, '.html', {})
 
 
 def sign_up(request):
-    """This is the view for the sign up page."""
+    """View function for the sign up page."""
     return render(request, '.html', {})
 
 
 def categories(request):
-    """This is the view for the categories page."""
+    """View function for the categories page."""
     return render(request, '.html', {})
 
 
 def reviews_and_ratings(request):
-    """This is the view for the customers review and ratings page."""
+    """View function for the customers review and ratings page."""
     return render(request, '.html', {})
