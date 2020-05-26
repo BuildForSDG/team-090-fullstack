5042bd5aa833d0ae001db4a648a1f996e038b46a
ardesp <belloshehu1@gmail.com>
2020-05-26 09:33:43 +0100

    Added test for valid and invalid suspension model instance

--- smartcity/fullstack/tests.py
+++ smartcity/fullstack/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
-from .models import Service, Category, ServiceProvider, CustomerProfile, Subscription
+from .models import Service, Category, ServiceProvider, CustomerProfile, Subscription, Suspension
 from cities_light.models import City, Country, Region
 from django.contrib.auth.models import User
 from django.utils import timezone
+import datetime
 # Create your tests here.
 
 
@@ -31,20 +32,20 @@ class ModelTestCase(TestCase):
                                          price=120, category=category)
 
         service_provider = ServiceProvider.objects.create(
-                                       business_name='Tailor1',
-                                       street_address='Adress1',
-                                       phone='080333333404',
-                                       service_rendered=service,
-                                       service_category=category,
-                                       picture='image.jpg',
-                                       description='Dress for both genders',
-                                       years_of_experience=12,
-                                       year_of_establishement=timezone.now().date(),
-                                       region=region,
-                                       city=city,
-                                       country=country,
-                                       supporting_document='', rating=4,
-                                       user=user)
+                            business_name='Tailor1',
+                            street_address='Adress1',
+                            phone='080333333404',
+                            service_rendered=service,
+                            service_category=category,
+                            picture='image.jpg',
+                            description='Dress for both genders',
+                            years_of_experience=12,
+                            year_of_establishement=timezone.now().date(),
+                            region=region,
+                            city=city,
+                            country=country,
+                            supporting_document='', rating=4,
+                            user=user)
 
         customer_profile = CustomerProfile.objects.create(user=user,
                                                           country=country,
@@ -73,11 +74,36 @@ class ModelTestCase(TestCase):
         service_provider = ServiceProvider.objects.get(business_name='Tailor1')
         subcription = Subscription.objects.get(
                         service_provider=service_provider)
-        self.assertEquals(subcription.service_provider.bussiness_name, 'Bello')
+        self.assertNotEquals(
+                        subcription.service_provider.business_name, 'Bello')
 
     def test_invalid_subscription_model(self):
         service_provider = ServiceProvider.objects.get(business_name='Tailor1')
         subscription = Subscription.objects.get(
                         service_provider=service_provider)
-        self.assertEquals(subscription.service_provider.bussiness_name,
+        self.assertEquals(subscription.service_provider.business_name,
                           'Tailor1')
+
+    def test_valid_suspension_model(self):
+        service_provider = ServiceProvider.objects.get(business_name='Tailor1')
+        suspension = Suspension.objects.create(
+            service_provider=service_provider,
+            suspension_start_date=timezone.now(),
+            suspension_end_date=timezone.now() + datetime.timedelta(days=2))
+
+        service_provider = ServiceProvider.objects.get(business_name='Tailor1')
+        suspension = Suspension.objects.get(
+                        service_provider=service_provider)
+        self.assertIs(suspension.suspension_using_correct_date(), True)
+
+    def test_invalid_suspension_model(self):
+        service_provider = ServiceProvider.objects.get(business_name='Tailor1')
+        suspension = Suspension.objects.create(
+            service_provider=service_provider,
+            suspension_start_date=timezone.now()+datetime.timedelta(days=2),
+            suspension_end_date=timezone.now())
+
+        service_provider = ServiceProvider.objects.get(business_name='Tailor1')
+        suspension = Suspension.objects.get(
+                        service_provider=service_provider)
+        self.assertIs(suspension.suspension_using_correct_date(), False)
