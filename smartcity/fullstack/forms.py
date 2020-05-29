8031abb4cf709db97c7c994719f321080ccb7fcf
ardesp <belloshehu1@gmail.com>
2020-05-29 11:00:17 +0100

    Added docstring

--- smartcity/fullstack/forms.py
+++ smartcity/fullstack/forms.py
@@ -5,11 +5,13 @@ from .models import ServiceProvicer, CustomerProfile
 
 class ServiceProviderProfileForm(ModelForm):
     class Meta:
+        """Generate fields from ServiceProvider model"""
         model = ServiceProvicer
         exclude = ['rating', 'user']
 
 
 class CustomerProfileForm(ModelForm):
     class Meta:
+        """Generate fields from CustomerProfile model"""
         model = CustomerProfile
         exclude = ['user']
