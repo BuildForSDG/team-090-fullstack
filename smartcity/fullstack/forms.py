732f3ccc3bbf76df67c50bfdce9a8a5f157c9cb4
ardesp <belloshehu1@gmail.com>
2020-05-29 10:52:15 +0100

    Added CustomerProfileForm and ServiceProviderProfileForm

--- smartcity/fullstack/forms.py
+++ smartcity/fullstack/forms.py
@@ -1,3 +1,15 @@
 from django.forms import ModelForm
-from cities_light.models import Country, Region, City
-from .models import ServiceProvicer, CustomerProfile 
+# from cities_light.models import Country, Region, City
+from .models import ServiceProvicer, CustomerProfile
+
+
+class ServiceProviderProfileForm(ModelForm):
+    class Meta:
+        model = ServiceProvicer
+        exclude = ['rating', 'user']
+
+
+class CustomerProfileForm(ModelForm):
+    class Meta:
+        model = CustomerProfile
+        exclude = ['user']
