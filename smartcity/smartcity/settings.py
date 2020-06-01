57e94fb3f8439c6836b3846ae1073ddaf0aab42e
ardesp <belloshehu1@gmail.com>
2020-06-01 14:00:22 +0100

    Added static dynamic files directory

--- smartcity/smartcity/settings.py
+++ smartcity/smartcity/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL ='/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 # Settings for django-bootstrap3
 BOOTSTRAP4 = {
  'include_jquery': True,
