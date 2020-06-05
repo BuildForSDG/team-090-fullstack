8f11e7051041e9a21b57963a36b3028fa720b1b9
ardesp <belloshehu1@gmail.com>
2020-06-05 09:07:21 +0100

    Added Document model. Added document_type, currency, and price fields in ServiceProfvider model .

--- smartcity/fullstack/models.py
+++ smartcity/fullstack/models.py
@@ -3,7 +3,10 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from cities_light.models import City, Region, Country
 from phonenumber_field.modelfields import PhoneNumberField
+from .my_currency import get_currencies_tuple
 
+
+CURRENCY = get_currencies_tuple()
 # Create your models here.
 
 
@@ -19,8 +22,15 @@ class Category(models.Model):
 class Service(models.Model):
     """ Model for services rendered"""
     name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Document(models.Model):
+    """Model for document required from service provicer."""
+    name = models.CharField(max_length=150)
 
     def __str__(self):
         return f'{self.name}'
@@ -31,21 +41,30 @@ class ServiceProvider(models.Model):
     business_name = models.CharField(max_length=100)
     street_address = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
-    service_rendered = models.ForeignKey(Service, on_delete=models.CASCADE)
     service_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    service_rendered = models.ForeignKey(Service, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=100, choices=CURRENCY,
+                                default=CURRENCY[98])
     picture = models.ImageField(upload_to='images/')
     description = models.TextField(max_length=200)
     years_of_experience = models.IntegerField()
     year_of_establishement = models.DateField(default=timezone.now)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    supporting_document = models.FileField(upload_to='documents/')
+    supporting_document = models.FileField(upload_to='documents/', blank=True)
+    document_type = models.ForeignKey(Document, on_delete=models.CASCADE,
+                                      blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     approval = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.business_name}'
+
+    def document_required(self):
+        if self.service_category.document_required:
+            self.supporting_document.blank = False
 
 
 class CustomerProfile(models.Model):
