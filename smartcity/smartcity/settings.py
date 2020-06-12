f3772f161853754b397019c1daff7c2bff9d1768
ardesp <belloshehu1@gmail.com>
2020-06-12 22:40:42 +0100

    Added deployment configuration code

--- smartcity/smartcity/settings.py
+++ smartcity/smartcity/settings.py
@@ -131,3 +131,17 @@ MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 BOOTSTRAP4 = {
  'include_jquery': True,
  }
+
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+    # Honor the 'X-Forwarded-Proto' heaer for request.is_secure()
+    SECURE_PROXY_SSL_HEADER=('HTT_X_FORWARDED_PROTO', 'https')
+    # Allow all host headers.
+    ALLOWED_HOSTS = ['*']
+    # Static asset configuration.
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
