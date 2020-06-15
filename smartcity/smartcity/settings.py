39e9b5950531663c9ca03298382cd1ca9ec07e9c
ardesp <belloshehu1@gmail.com>
2020-06-15 12:47:08 +0100

    added heroku app and local url in ALLOWED_HOSTS

--- smartcity/smartcity/settings.py
+++ smartcity/smartcity/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a)*e&77+krsx_qxnlbsyh(e04c3ce9$are4xj08@a@rzuiph8m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smartcity090.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
