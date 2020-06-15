a79d848a86e22ae39a90f13fbf08e9592d861c50
ardesp <belloshehu1@gmail.com>
2020-06-15 15:36:24 +0100

    Removed Cling

--- smartcity/smartcity/wsgi.py
+++ smartcity/smartcity/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartcity.settings')
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
