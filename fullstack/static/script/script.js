317624387e5f03aad60ae89865eb6a77d686e15b
ardesp <belloshehu1@gmail.com>
2020-07-02 10:05:47 +0100

    Removed unused html and javascript.

--- fullstack/static/script/script.js
+++ fullstack/static/script/script.js
@@ -82,9 +82,7 @@ $("#search-form").submit(function(){
         },
       }
     );
-
   }
-  
   return false;
 });
   // get states for the selected country 
@@ -129,63 +127,10 @@ $("#state, #id_region").change(function(){
           $("#city, #id_city").append('<option id="selected-city" value="'+ data.cities[i]['id'] +'">'+ data.cities[i]['city'] +'</option>');
         }
       },
-      error: function(){
-        alert('erro');
+      error: function(data){
+        $("#alert").attr("class","alert alert-success text-danger").show().text("Failed to load cities.").delay(700).hide(400);
       }
     }
   );
 });
-  var myLatitude = 0;
-  var myLongitude = 0;
-  function showPosition(position) {
-    myLatitude = position.coords.latitude;
-    myLongitude = position.coords.longitude; 
-  }
-  var x = $("#error");
-  function getLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-      } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
-      }
-    }
-    
-    function showError(error) {
-      switch(error.code) {
-        case error.PERMISSION_DENIED:
-          x.innerHTML = "User denied the request for Geolocation.";
-          break;
-        case error.POSITION_UNAVAILABLE:
-          x.innerHTML = "Location information is unavailable.";
-          break;
-        case error.TIMEOUT:
-          x.innerHTML = "The request to get user location timed out.";
-          break;
-        case error.UNKNOWN_ERROR:
-          x.innerHTML = "An unknown error occurred.";
-          break;
-      default:
-          x.innerHTML = " ";
-      }
-    }
-  getLocation();
-  if(myLatitude === 0 ){
-      myLatitude = 11.83537;
-      myLongitude = 13.15166;
-  }
-  mapboxgl.accessToken = "pk.eyJ1IjoiYXJkZXNwIiwiYSI6ImNrYjZsY25pYjBwdXoyeHF2MXJoYzh5Z2YifQ.0l2qyChIYTHzZSTL0umdAg";
-  var map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
-  center: [myLongitude, myLatitude],
-  zoom: 4
-  });
-  map.addControl(
-  new MapboxGeocoder({
-  accessToken: mapboxgl.accessToken,
-  mapboxgl: mapboxgl
-  })
-  );
-
-  var marker = new mapboxgl.Marker().setLngLat([myLongitude,myLatitude]).addTo(map);
 });
